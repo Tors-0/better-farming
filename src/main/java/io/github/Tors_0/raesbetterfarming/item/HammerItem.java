5b9647c0aaf270a7088a87ef2f09f4c91e09675b
rae <raej0000@gmail.com>
2024-01-08 22:18:08 -0800

    optimize inheritance

--- src/main/java/io/github/Tors_0/raesbetterfarming/item/HammerItem.java
+++ src/main/java/io/github/Tors_0/raesbetterfarming/item/HammerItem.java
@@ -1,37 +1,14 @@
 package io.github.Tors_0.raesbetterfarming.item;
 
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Multimap;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.*;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.tag.BlockTags;
-import net.minecraft.tag.TagKey;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
-public class HammerItem extends ToolItem implements Vanishable {
-
-    private final TagKey<Block> effectiveBlocks;
-    protected final float miningSpeed;
-    private final float attackDamage;
-    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+public class HammerItem extends MiningToolItem implements Vanishable {
     public HammerItem(float toolBaseDamage, float attackSpeed, ToolMaterial material, Settings settings) {
-        super(material, settings);
-        this.effectiveBlocks = BlockTags.PICKAXE_MINEABLE;
-        this.miningSpeed = material.getMiningSpeedMultiplier();
-        this.attackDamage = toolBaseDamage + material.getAttackDamage();
-        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Tool modifier", (double)this.attackDamage, EntityAttributeModifier.Operation.ADDITION));
-        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Tool modifier", (double)attackSpeed, EntityAttributeModifier.Operation.ADDITION));
-        this.attributeModifiers = builder.build();
+        super(toolBaseDamage,attackSpeed,material,BlockTags.PICKAXE_MINEABLE, settings);
     }
 
     @Override
@@ -39,42 +16,10 @@ public class HammerItem extends ToolItem implements Vanishable {
         return Ingredient.ofItems(Items.IRON_INGOT).test(ingredient);
     }
 
-    public boolean isSuitableFor(BlockState state) {
-        int i = this.getMaterial().getMiningLevel();
-        if (i < 3 && state.isIn(BlockTags.NEEDS_DIAMOND_TOOL)) {
-            return false;
-        } else if (i < 2 && state.isIn(BlockTags.NEEDS_IRON_TOOL)) {
-            return false;
-        } else {
-            return i < 1 && state.isIn(BlockTags.NEEDS_STONE_TOOL) ? false : state.isIn(this.effectiveBlocks) || state.isOf(Blocks.COBWEB);
-        }
-    }
-    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
-        return state.isIn(this.effectiveBlocks) ? this.miningSpeed : 1.0F;
-    }
-
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        stack.damage(1, attacker, (e) -> { // unlike a regular hoe, this only takes 1 durability for attacks as it can be a weapon also
+        stack.damage(1, attacker, (e) -> { // unlike a regular pickaxe, this only takes 1 durability for attacks as it can be a weapon also
             e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
         });
         return true;
-    }
-
-    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-        if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
-            stack.damage(1, miner, (e) -> {
-                e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
-            });
-        }
-
-        return true;
-    }
-
-    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
-        return slot == EquipmentSlot.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(slot);
-    }
-
-    public float getAttackDamage() {
-        return this.attackDamage;
     }
 }
